8ca375d21e406c8685cac2926c6463fa9fa248c9
Bohuslav Kabrda <bkabrda@redhat.com>
2013-01-29 14:35:56 +0100

    Some tuning of Python assistant, scaffold the Django assistant

--- devassistant/assistants/python.py
+++ devassistant/assistants/python.py
@@ -1,9 +1,16 @@
-from devassistant.assistant_base import AssistantBase
+from devassistant import argument
+from devassistant import assistant_base
 
-class PythonAssistant(AssistantBase):
-    name = 'base'
-    verbose_name = 'Base'
-    needs_sudo = False
+class PythonAssistant(assistant_base.AssistantBase):
+    name = 'python'
+    verbose_name = 'Python'
+
+    args = []
+    usage_string_fmt = 'Usage of {verbose_name}:'
+
+class DjangoAssistant(assistant_base.PythonAssistant):
+    name = 'django'
+    verbose_name = 'Django'
 
     args = []
     usage_string_fmt = 'Usage of {verbose_name}:'
