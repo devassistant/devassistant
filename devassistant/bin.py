10fa0fdc055378bf89f7e33a117e6a8bf3bf0208
Bohuslav Kabrda <bkabrda@redhat.com>
2013-01-29 15:56:46 +0100

    Basic setup code for bin, proof of concept that the assistant chain is loaded properly

--- devassistant/bin.py
+++ devassistant/bin.py
@@ -1,2 +1,18 @@
+from devassistant import argument
+from devassistant import assistant_base
+from devassistant import settings
+
+# for now, import Assistants by hand, but we may want to do this automatically
+from devassistant.assistants import python
+
+class MainAssistant(assistant_base.AssistantBase):
+    name = 'main'
+    verbose_name = 'Main'
+
+    usage_string_fmt = 'Usage of {verbose_name}'
+
+    args = [argument.Argument(settings.SUBASSISTANTS_STRING,
+                              subassistants={python.PythonAssistant.name: python.PythonAssistant})]
+
 def main():
-    pass
+    parsed_args = MainAssistant().get_argument_parser().parse_args()
