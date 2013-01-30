e03411c6940da26610f489c598a739dc4712cf53
Bohuslav Kabrda <bkabrda@redhat.com>
2013-01-30 14:25:55 +0100

    Packages installation for FlaskAssistant

--- devassistant/assistants/python.py
+++ devassistant/assistants/python.py
@@ -63,3 +63,22 @@ class FlaskAssistant(PythonAssistant):
         if path_exists:
             errors.append(path_exists)
         return errors
+
+    def prepare(self, **kwargs):
+        self.to_install = []
+
+        # TODO: this should be substituted by a yum group
+        for pkg in ['python-flask', 'python-flask-sqlalchemy', 'python-flask-wtf']:
+            if not RPMHelper.is_rpm_present(pkg):
+                self.to_install = []
+
+        if self.to_install:
+            self.needs_sudo = True
+
+    def run(self, **kwargs):
+        if self.to_install:
+            YUMHelper.install(*self.to_install)
+            for pkg in self.to_install:
+                RPMHelper.was_rpm_installed(pkg)
+
+        logger.info('Kickstarting a Flask project under {0}'.format(kwargs['name'])
