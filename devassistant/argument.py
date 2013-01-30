d2d24403200992eadbe5bbb21198aacd0646fdd9
Bohuslav Kabrda <bkabrda@redhat.com>
2013-01-30 13:09:07 +0100

    Pass keyword arguments properly to add_argument

--- devassistant/argument.py
+++ devassistant/argument.py
@@ -1,8 +1,7 @@
 class Argument(object):
     def __init__(self, *args, **kwargs):
         self.flags = args
-        for k, v in kwargs.items():
-            setattr(self, k, v)
+        self.kwargs = kwargs
 
     def add_argument_to(self, parser):
-        parser.add_argument(*self.flags)
+        parser.add_argument(*self.flags, **self.kwargs)
