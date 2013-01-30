e07620264caf1425592f844f97a4053a3e7fb3e3
Bohuslav Kabrda <bkabrda@redhat.com>
2013-01-30 13:38:18 +0100

    Start working on FlaskAssistant

--- devassistant/assistants/python.py
+++ devassistant/assistants/python.py
@@ -7,7 +7,7 @@ from devassistant.command_helpers import PathHelper, RPMHelper, YUMHelper
 
 class PythonAssistant(assistant_base.AssistantBase):
     def get_subassistants(self):
-        return [DjangoAssistant]
+        return [DjangoAssistant, FlaskAssistant]
 
     name = 'python'
     verbose_name = 'Python'
@@ -49,3 +49,18 @@ class DjangoAssistant(PythonAssistant):
             YUMHelper.install('python-django')
             django_rpm = RPMHelper.is_rpm_present('python-django')
             logger.info('Installed %s', django_rpm)
+
+class FlaskAssistant(PythonAssistant):
+    name = 'flask'
+    verbose_name = 'Flask'
+
+    args = [argument.Argument('-n', '--name', required=True)]
+    usage_string_fmt = 'Usage of {verbose_name}:'
+
+    def errors(self, **kwargs):
+        errors = []
+        if PathHelper.path_exists(kwargs['name']):
+            msg = 'Path "{0}" exists, cannot create a new Flask project there.'.format(kwargs['name'])
+            logger.error(msg)
+            errors.append(msg)
+        return errors
