65f9c25bfdfcb446b92f7f33a838e456fa20bf2f
Bohuslav Kabrda <bkabrda@redhat.com>
2013-01-30 13:32:58 +0100

    Path runner now handles errors from assistant methods

--- devassistant/path_runner.py
+++ devassistant/path_runner.py
@@ -1,12 +1,16 @@
+from devassistant.logger import logger
+
 class PathRunner(object):
     def __init__(self, path, parsed_args):
         self.path = path
         self.parsed_args = parsed_args
 
     def _run_path_errors(self):
+        errors = []
         for a in self.path:
             if 'errors' in vars(a.__class__):
-                a.errors(**vars(self.parsed_args))
+                errors.extend(a.errors(**vars(self.parsed_args)))
+        return errors
 
     def _run_path_prepare(self):
         for a in self.path:
@@ -19,6 +23,9 @@ class PathRunner(object):
                 a.run(**vars(self.parsed_args))
 
     def run(self):
-        self._run_path_errors()
+        errors = self._run_path_errors()
+        if errors:
+            logger.error('Can\'t continue because of errors, aborting.')
+            return
         self._run_path_prepare()
         self._run_path_run()
