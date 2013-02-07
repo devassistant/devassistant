846ff0f65a4e8e847edae185cb8e075d8f8e7261
Bohuslav Kabrda <bkabrda@redhat.com>
2013-02-07 12:15:09 +0100

    Test loading assistant classes from yaml

--- test/test_yaml_loader.py
+++ test/test_yaml_loader.py
@@ -1,9 +1,33 @@
 import os
+
 import pytest
+import yaml
 
 from devassistant.yaml_loader import YamlLoader
 
 class TestYamlLoader(object):
     def setup_method(self, method):
-        self.yl = YamlLoader()
-        yl.yaml_dir = os.path.join(os.path.dirname(__file__), 'fixtures')
+        self.yl = YamlLoader
+        self.yl.yaml_dir = os.path.join(os.path.dirname(__file__), 'fixtures')
+
+    def load_yaml_from_fixture(self, fixture):
+        fixture_path = os.path.join(self.yl.yaml_dir, '{0}.yaml'.format(fixture))
+        fhandler = open(fixture_path)
+        return yaml.load(fhandler)
+
+    def test_class_from_yaml(self):
+        y = self.load_yaml_from_fixture('c')
+        klass = self.yl.class_from_yaml(y)
+        
+        assert klass.name == 'c'
+        assert klass.fullname == 'C Language Tool'
+        assert len(klass.args) == 1
+        assert klass._invoke_if_subassistant_used == False
+        assert klass._dependencies == {'rpm': ['rpm']}
+        assert klass._fail_if == [{'cl': 'ls /'}]
+        assert klass._files == {
+            'clientc': {'source': 'templates/c/client.c'},
+            'serverc': {'source': 'templates/c/server.c'}
+        }
+        assert klass._subassistants == ['d', 'e']
+        assert klass._run == [{'cl': 'ls foo/bar'}]
