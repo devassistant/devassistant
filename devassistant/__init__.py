3c459ae48e3f861165f0d1730bd249d4ecd26483
Bohuslav Kabrda <bkabrda@redhat.com>
2013-02-12 15:25:34 +0100

    Handle sigint nicely

---
+++ devassistant/__init__.py
@@ -0,0 +1,11 @@
+import signal
+
+def signal_handler(signal, frame):
+    import sys
+    print 'Devassistant received SIGINT, exiting...'
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+del signal
+del signal_handler
