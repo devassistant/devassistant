7805739ad52a6f827149d6d52cf94e6bc21ee653
Bohuslav Kabrda <bkabrda@redhat.com>
2013-02-12 15:38:54 +0100

    The python2 habbits are persistent

--- devassistant/__init__.py
+++ devassistant/__init__.py
@@ -2,7 +2,7 @@ import signal
 
 def signal_handler(signal, frame):
     import sys
-    print 'Devassistant received SIGINT, exiting...'
+    print('Devassistant received SIGINT, exiting...')
     sys.exit(0)
 
 signal.signal(signal.SIGINT, signal_handler)
