d58edd1e3203446bc7ec299268c9198197012778
Bohuslav Kabrda <bkabrda@redhat.com>
2013-02-22 09:08:18 +0100

    No logging handler by default, cli/gui now have to register one or more. Fixes #2

--- devassistant/cli/cli_runner.py
+++ devassistant/cli/cli_runner.py
@@ -1,3 +1,6 @@
+import logging
+import sys
+
 from devassistant.cli import argparse_generator
 from devassistant.cli import path_runner
 from devassistant import exceptions
@@ -6,7 +9,16 @@ from devassistant import settings
 
 class CliRunner(object):
     @classmethod
+    def register_console_logging_handler(cls):
+        console_handler = logging.StreamHandler(stream=sys.stdout)
+        console_formatter = logging.Formatter('%(levelname)s - %(message)s')
+        console_handler.setFormatter(console_formatter)
+        console_handler.setLevel(logging.INFO)
+        logger.logger.addHandler(console_handler)
+
+    @classmethod
     def run_assistant(cls, assistant):
+        cls.register_console_logging_handler()
         ch = assistant.get_subassistant_chain()
         parsed_args = argparse_generator.ArgparseGenerator.generate_argument_parser(ch).parse_args()
         path = assistant.get_selected_subassistant_path(**vars(parsed_args))
