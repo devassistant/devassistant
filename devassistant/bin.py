65fc00d2f75a33e1a971c65b6ecefea319c5bd20
Bohuslav Kabrda <bkabrda@redhat.com>
2013-03-13 09:28:29 +0100

    Don't try to load PythonAssistant from previously deleted file

--- devassistant/bin.py
+++ devassistant/bin.py
@@ -2,13 +2,9 @@ from devassistant import assistant_base
 from devassistant import cli
 from devassistant import yaml_loader
 
-# for now, import Assistants by hand, but we may want to do this automatically
-from devassistant.assistants import python
-
 class MainAssistant(assistant_base.AssistantBase):
     def get_subassistants(self):
-        sa = [python.PythonAssistant]
-        sa.extend(yaml_loader.YamlLoader.get_top_level_assistants())
+        sa = yaml_loader.YamlLoader.get_top_level_assistants()
         return sa
 
     name = 'main'
