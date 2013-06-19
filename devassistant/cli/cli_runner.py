73a2c345de9ccdeeaa4ff88ccfbcb98cc1197835
Bohuslav Kabrda <bkabrda@redhat.com>
2013-06-19 12:36:17 +0200

    stream is a non-keyword arg to StreamHandler on Python 2.6

--- devassistant/cli/cli_runner.py
+++ devassistant/cli/cli_runner.py
@@ -9,7 +9,7 @@ from devassistant import path_runner
 class CliRunner(object):
     @classmethod
     def register_console_logging_handler(cls):
-        console_handler = logging.StreamHandler(stream=sys.stdout)
+        console_handler = logging.StreamHandler(sys.stdout)
         console_formatter = logging.Formatter('%(levelname)s - %(message)s')
         console_handler.setFormatter(logger.DevassistantClFormatter())
         console_handler.setLevel(logging.INFO)
