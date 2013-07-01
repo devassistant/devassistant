412fd0d2d7c313ec5197ca3fe4f01f5d2a4ee9a2
Bohuslav Kabrda <bkabrda@redhat.com>
2013-07-01 11:54:10 +0200

    Add manpage for devassistant-prepare

--- manpages/man_page_generate.sh
+++ manpages/man_page_generate.sh
@@ -10,6 +10,12 @@ MAN_PAGE_MODIFY="devassistant-modify.1"
 MAN_PAGE_MODIFY_FIRST="devassistant-modify.1.head"
 MAN_PAGE_MODIFY_SECOND="devassistant-modify.1.tail"
 
+PROG_PREPARE="../devassistant-prepare.py"
+MAN_PAGE_PREPARE="devassistant-prepare.1"
+MAN_PAGE_PREPARE_FIRST="devassistant-prepare.1.head"
+MAN_PAGE_PREPARE_SECOND="devassistant-prepare.1.tail"
+
+# TODO: we should have just one "parse" function for all assistant types
 function parse () {
     local CMD=$@
     RET=`$CMD --help | tail -n 1`
@@ -53,12 +59,39 @@ function parse_modify () {
         parse_modify $CMD $ass
     done
 }
+
+function parse_prepare () {
+    local CMD=$@
+    RET=`$CMD --help | tail -n 1`
+    ASSISTANTS=`echo ${RET:3:(-1)}`
+    has_subass=`echo ${RET:2:1}`
+    if [ x"$has_subass" != x"{" ]; then
+        RET=`$CMD --help | head -n 2`
+        RET=$(echo $RET | tr "\n" "\n")
+        RES=`echo ${RET#*}`
+        RES=`echo ${RES#usage: devassistant-prepare.py}`
+        echo "Found command line possibilities(devassistant-prepare): $RES"
+        echo "\fBdevassistant-modify \fP $RES" >> $MAN_PAGE_PREPARE
+        echo ".br" >> $MAN_PAGE_PREPARE
+        return 
+    fi
+    arr=$(echo $ASSISTANTS | tr "," "\n")
+    for ass in $arr
+    do
+        parse_prepare $CMD $ass
+    done
+}
+
+
 echo "Find all assistants"
 if [ -f $MAN_PAGE.gz ]; then
     rm $MAN_PAGE.gz
 fi
 if [ -f $MAN_PAGE_MODIFY.gz ]; then
     rm $MAN_PAGE_MODIFY.gz
+fi
+if [ -f $MAN_PAGE_PREPARE.gz ]; then
+    rm $MAN_PAGE_PREPARE.gz
 fi
 cat $MAN_PAGE_FIRST > $MAN_PAGE
 parse $PROG
@@ -67,3 +100,7 @@ cat $MAN_PAGE_SECOND >> $MAN_PAGE
 cat $MAN_PAGE_MODIFY_FIRST > $MAN_PAGE_MODIFY
 parse_modify $PROG_MODIFY
 cat $MAN_PAGE_MODIFY_SECOND >> $MAN_PAGE_MODIFY
+
+cat $MAN_PAGE_PREPARE_FIRST > $MAN_PAGE_PREPARE
+parse_prepare $PROG_PREPARE
+cat $MAN_PAGE_PREPARE_SECOND >> $MAN_PAGE_PREPARE
