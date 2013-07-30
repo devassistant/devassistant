4208a6bce649f2afdc087ae1c403634bee7b7e09
Bohuslav Kabrda <bkabrda@redhat.com>
2013-07-30 12:59:42 +0200

    Return proper return value from zenity_wrapper

--- devassistant/zenity_wrapper.sh
+++ devassistant/zenity_wrapper.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 sleep .1 && wmctrl -a Information -b add,above &
-TEXT=`WINDOWID=$(xwininfo -root -int | awk '/xwininfo:/{print $4}') \
-    zenity "$@" &`
+TEXT=$(WINDOWID=$(xwininfo -root -int | awk '/xwininfo:/{print $4}') \
+	zenity "$@")
+retval=$?
+echo $retval
 echo $TEXT
+exit $retval
