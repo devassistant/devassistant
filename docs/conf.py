c7847fb6fba099d1a998d72beca7d15729a54326
Bohuslav Kabrda <bkabrda@redhat.com>
2013-08-05 14:14:34 +0200

    Get docs version from devassistant dynamically, this has to do it.

--- docs/conf.py
+++ docs/conf.py
@@ -13,15 +13,15 @@
 
 import sys, os
 
-devassistant_dir = os.path.abspath(os.path.join('..', 'devassistant'))
-sys.path.append(devassistant_dir)
-
-from devassistant import version as da_version
-
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
+
+devassistant_dir = os.path.abspath('..')
+sys.path.insert(0, devassistant_dir)
+
+from devassistant import version as da_version
 
 # -- General configuration -----------------------------------------------------
 
