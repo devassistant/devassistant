b5953c06612d7c07e169b37046dffb8160d7889a
Petr Hracek <phracek@redhat.com>
2013-09-03 15:01:44 +0200

    Added function for DevAssistant YAML creator

--- devassistant/gui/__init__.py
+++ devassistant/gui/__init__.py
@@ -1,6 +1,7 @@
 import sys
 
 from devassistant.gui import mainWindow
+from devassistant.gui import creatorWindow
 
 def run_gui():
     try:
@@ -11,3 +12,13 @@ def run_gui():
         sys.exit(1)
 
     mainWindow.mainWindow()
+
+def run_yaml_gui():
+    try:
+        from gi.repository import Gtk
+    except RuntimeError, e:
+        print _("Developer Assistant GUI requires a running X server.")
+        print "%s: %r" % (e.__class__.__name__, str(e))
+        sys.exit(1)
+
+    creatorWindow.creatorWindow()
