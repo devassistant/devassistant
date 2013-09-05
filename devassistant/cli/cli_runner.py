0be7e4e847e6d30ba11cb3711af3347e6c94fafd
Bohuslav Kabrda <bkabrda@redhat.com>
2013-09-05 08:39:27 +0200

    Docstrings and minor tweaks for cli runner

--- devassistant/cli/cli_runner.py
+++ devassistant/cli/cli_runner.py
@@ -12,20 +12,28 @@ from devassistant import settings
 
 class CliRunner(object):
     @classmethod
-    def register_console_logging_handler(cls):
+    def register_console_logging_handler(cls, lgr):
+        """Registers console logging handler to given logger."""
         console_handler = logging.StreamHandler(sys.stdout)
         console_handler.setFormatter(logger.DevassistantClFormatter())
         console_handler.setLevel(logging.INFO)
-        logger.logger.addHandler(console_handler)
+        lgr.addHandler(console_handler)
 
     @classmethod
     def run(cls):
-        cls.register_console_logging_handler()
+        """Runs the whole cli:
+
+        1. Registers console logging handler
+        2. Creates argparser from all assistants and actions
+        3. Parses args and decides what to run
+        4. Runs a proper assistant or action
+        """
+        cls.register_console_logging_handler(logger.logger)
         cls.inform_of_short_bin_name(sys.argv[0])
         top_assistant = bin.TopAssistant()
-        ch = top_assistant.get_subassistant_tree()
+        tree = top_assistant.get_subassistant_tree()
         argparser = argparse_generator.ArgparseGenerator.\
-                        generate_argument_parser(ch, actions=actions)
+                        generate_argument_parser(tree, actions=actions)
         parsed_args = argparser.parse_args()
         first_subparser = vars(parsed_args)[settings.SUBASSISTANT_N_STRING.format(0)]
         if first_subparser in actions:
@@ -41,6 +49,9 @@ class CliRunner(object):
 
     @classmethod
     def inform_of_short_bin_name(cls, binary):
+        """Historically, we had "devassistant" binary, but we chose to go with
+        shorter "da". We still allow "devassistant", but we recommend using "da".
+        """
         binary = os.path.splitext(os.path.basename(binary))[0]
         if binary != 'da':
             msg = '"da" is the preffered way of running "{binary}".'.format(binary=binary)
