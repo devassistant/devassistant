6bdc4018a96d7509b93c73eafbc612d2bb26b0db
Bohuslav Kabrda <bkabrda@redhat.com>
2013-09-05 08:22:18 +0200

    Docstrings for cache.

--- devassistant/cache.py
+++ devassistant/cache.py
@@ -12,7 +12,44 @@ from devassistant import yaml_loader
 from devassistant import yaml_snippet_loader
 
 class Cache(object):
+    """Representation of DevAssistant cache file.
+    Cache is stored in yaml file between devassistant invocations. Once it is loaded,
+    it has following structure:
+
+    # type of assistants
+    {'creator':
+        # a single cached assistant
+        {'c':
+            # attributes of this assistant needed to compose cli/gui
+            {'attrs':
+                # args of this assistant (if snippet is used, they're stored expanded)
+                {'args':
+                    {'foo': {'flags': ['-f', '--foo'],
+                             'help': 'Help for foo parameter.'}},
+                 'description': 'C Language Tool description...',
+                 'fullname': 'C Language Tool'},
+            # snippets that this assistant depends on with their last seen ctimes
+            'snippets': {'somesnip': 11111111111},
+            # last seen ctime of this assistant
+            'ctime': 111111111111,
+            # source file of this assistant
+            'source': '/foo/bar/assistants/creator/c.yaml',
+            # hierarchy of subassistants of this assistant
+            'subhierarchy': {'d': {...}},
+     'modifier': {...},
+     'preparer': {...},
+     # version of devassistant that cache has been created with
+     'version': VERSION}
+    """
+
     def __init__(self, cache_file=settings.CACHE_FILE):
+        """Inits a cache objects with given cache_file. Creates the cache file if
+        it doesn't exist. If cache_file exists, but was created with different
+        DevAssistant version, it gets deleted.
+
+        Args:
+            cache_file: cache file to use
+        """
         self.cache_file = cache_file
         # snippets are shared across many assistants, so we remember their ctimes
         # here, because doing it again for each assistant would be very costly
@@ -34,6 +71,13 @@ class Cache(object):
             f.close()
 
     def refresh_role(self, role, file_hierarchy):
+        """Checks and refreshes (if needed) all assistants with given role.
+
+        Args:
+            role: role of assistants to refresh
+            file_hierarchy: hierarchy as returned by devassistant.yaml_assistant_loader.\
+                            YamlAssistantLoader.get_assistants_file_hierarchy
+        """
         if not role in self.cache:
             self.cache[role] = {}
         was_change = self._refresh_hierarchy_recursive(self.cache[role], file_hierarchy)
@@ -43,6 +87,19 @@ class Cache(object):
             cf.close()
 
     def _refresh_hierarchy_recursive(self, cached_hierarchy, file_hierarchy):
+        """Recursively goes through given corresponding hierarchies from cache and filesystem
+        and adds/refreshes/removes added/changed/removed assistants.
+
+        Args:
+            cached_hierarchy: the respective hierarchy part from current cache
+                              (for format see Cache class docstring)
+            file_hierarchy: the respective hierarchy part from filesystem
+                            (for format see what refresh_role accepts)
+
+        Returns:
+            True if self.cache has been changed, False otherwise (doesn't write anything
+            to cache file)
+        """
         was_change = False
         cached_ass = set(cached_hierarchy.keys())
         new_ass = set(file_hierarchy.keys())
@@ -76,6 +133,21 @@ class Cache(object):
 
 
     def _ass_needs_refresh(self, cached_ass, file_ass):
+        """Checks if assistant needs refresh.
+
+        Assistant needs refresh iff any of following conditions is True:
+        - stored source file is different than given source file
+        - stored assistant ctime is lower than current source file ctime
+        - stored list of subassistants is different than given list of subassistants
+        - stored ctime of any of the snippets that this assistant uses to compose
+          args is lower than current ctime of that snippet
+
+        Args:
+            cached_ass: an assistant from cache hierarchy
+                        (for format see Cache class docstring)
+            file_ass: the respective assistant from filesystem hierarchy
+                      (for format see what refresh_role accepts)
+        """
         if cached_ass['source'] != file_ass['source']:
             return True
         if os.path.getctime(file_ass['source']) > cached_ass.get('ctime', 0.0):
@@ -89,6 +161,14 @@ class Cache(object):
         return False
 
     def _ass_refresh_attrs(self, cached_ass, file_ass):
+        """Completely refreshes cached assistant from file.
+
+        Args:
+            cached_ass: an assistant from cache hierarchy
+                        (for format see Cache class docstring)
+            file_ass: the respective assistant from filesystem hierarchy
+                      (for format see what refresh_role accepts)
+        """
         # we need to process assistant in custom way to see unexpanded args, etc.
         loaded_ass = yaml_loader.YamlLoader.load_yaml_by_path(file_ass['source'])
         _, attrs = loaded_ass.popitem()
@@ -115,6 +195,14 @@ class Cache(object):
                 cached_ass['attrs']['args'][argname] = argparams
 
     def _new_ass_hierarchy(self, file_ass):
+        """Returns a completely new cache hierarchy for given assistant file.
+
+        Args:
+             file_ass: the assistant from filesystem hierarchy to create cache hierarchy for
+                      (for format see what refresh_role accepts)
+        Returns:
+            the newly created cache hierarchy
+        """
         ret_struct = {'source': '',
                       'subhierarchy': {},
                       'attrs': {},
@@ -128,6 +216,17 @@ class Cache(object):
         return ret_struct
 
     def _get_snippet_ctime(self, snip_name):
+        """Returns and remembers (during this DevAssistant invocation) last ctime of given
+        snippet.
+
+        Calling ctime costs lost of time and some snippets, like common_args, are used widely,
+        so we don't want to call ctime bazillion times on them during one invocation.
+
+        Args:
+            snip_name: name of snippet to get ctime for
+        Returns:
+            ctime of the snippet
+        """
         if snip_name not in self.snip_ctimes:
             snippet = yaml_snippet_loader.YamlSnippetLoader.get_snippet_by_name(snip_name)
             self.snip_ctimes[snip_name] = os.path.getctime(snippet.path)
