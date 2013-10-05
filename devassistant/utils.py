f6f80b7ea2f3bb21412378b799c6499401b5a66d
Bohuslav Kabrda <bkabrda@redhat.com>
2013-10-05 11:31:02 +0200

    Argh... now fix it without a typo

--- devassistant/utils.py
+++ devassistant/utils.py
@@ -3,7 +3,7 @@ try: # ugly hack for using imp instead of importlib on Python <= 2.6
 except ImportError:
     import imp as importlib
     def import_module(name):
-        fp, pathname, description = importlib.find_module(name.replace('.', '/')
+        fp, pathname, description = importlib.find_module(name.replace('.', '/'))
         return importlib.load_module(name, fp, pathname, description)
     importlib.import_module = import_module
     del import_module
