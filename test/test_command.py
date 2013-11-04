7d8b397e32fcd715d7d4d77d7ed8f59687309b5d
Tomas Radej <tradej@redhat.com>
2013-11-04 14:00:09 +0100

    TestCommand

--- test/test_command.py
+++ test/test_command.py
@@ -2,7 +2,7 @@ import pytest
 
 from devassistant.command import Command
 
-class TestCommandFormatter(object):
+class TestCommand(object):
     files_dir = '/a/b/c'
     files = {'first': {'source': 'f/g'}, 'second': {'source': 's/t'}}
 
@@ -18,8 +18,13 @@ class TestCommandFormatter(object):
         arg_dict['__files_dir__'] = [self.files_dir]
         assert Command('cl', comm, arg_dict).format_str() == result
 
-    def test_format_str_handles_bool(self): 
-        # If command is false/true in yaml file, it gets coverted to False/True 
-        # which is bool object. format should handle this. 
+    def test_format_str_handles_bool(self):
+        # If command is false/true in yaml file, it gets coverted to False/True
+        # which is bool object. format should handle this.
         assert Command('cl', True, {}).format_str() == 'true'
         assert Command('cl', False, {}).format_str() == 'false'
+
+    def test_format_deep_returns_correct_type(self):
+        assert isinstance(Command('cl', {}).format_deep(), dict)
+        assert isinstance(Command('cl', []).format_deep(), list)
+        assert isinstance(Command('cl', 'foo').format_deep(), str)
