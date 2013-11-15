232ab75538fcad00cf47d48fc5ce17898a728dfd
Tomas Radej <tradej@redhat.com>
2013-11-15 11:09:47 +0100

    More tests for command_runners

--- test/test_command_runners.py
+++ test/test_command_runners.py
@@ -4,7 +4,7 @@ from flexmock import flexmock
 
 from devassistant.command import Command
 from devassistant.command_helpers import DialogHelper
-from devassistant.command_runners import AskCommandRunner
+from devassistant.command_runners import AskCommandRunner, CallCommandRunner
 from devassistant.exceptions import CommandException, YamlSyntaxError
 
 
@@ -47,7 +47,58 @@ class TestAskCommandRunner(object):
         (Command('foo', ''),    CommandException, 'No commands specified'),
         (Command('foo', 'bar'),   YamlSyntaxError, 'Not a proper variable name: b'),
         (Command('foo', ['bar']), YamlSyntaxError, 'Not a proper variable name: bar')])
-    def test_format_args(self, command, exception, exception_text):
+    def test_format_args_fails(self, command, exception, exception_text):
         with pytest.raises(exception) as excinfo:
             self.acr.format_args(command)
         assert exception_text in excinfo.value
+
+    def test_format_args_passes(self):
+        comm = Command('ask_password', ['$password', {'prompt': 'foo'}])
+        (var, fmtd) = self.acr.format_args(comm)
+        assert var == 'password'
+        assert fmtd['prompt'] == 'foo'
+
+
+class TestCallCommandRunner(object):
+    def setup_method(self, method):
+        self.ccr = CallCommandRunner
+
+    def test_matches(self):
+        assert self.ccr.matches(Command('call', None))
+        assert self.ccr.matches(Command('use', None))
+        assert not self.ccr.matches(Command('foo', None))
+
+    @pytest.mark.parametrize('command', ['self', 'super'])
+    def test_is_snippet_call_fails(self, command):
+        assert not self.ccr.is_snippet_call(command)
+        assert not self.ccr.is_snippet_call('{}.foo'.format(command))
+
+    def test_is_snippet_call_passes(self):
+        assert self.ccr.is_snippet_call('foo')
+
+    # TODO test other methods
+
+class TestClCommandRunner(object):
+    pass
+
+class TestDependenciesCommandRunner(object):
+    pass
+
+class TestDotDevassistantCommandRunner(object):
+    pass
+
+class TestGitHubAuth(object):
+    # This should probably go to helpers or somewhere
+    pass
+
+class TestGitHubCommandRunner(object):
+    pass
+
+class TestLogCommandRunner(object):
+    pass
+
+class TestSaveProjectCommandRunner(object):
+    pass
+
+class TestSCLCommandRunner(object):
+    pass
