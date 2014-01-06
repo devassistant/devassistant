18387f25536360b8052c1edec5899b9c9add0df0
Miro Hrončok <miro@hroncok.cz>
2014-01-06 23:28:25 +0100

    daploader: Add license and real data to setup.py

--- setup.py
+++ setup.py
@@ -9,18 +9,24 @@ except:
 setup(
     name='daploader',
     version='0.0.1',
-    description='Short description',
-    long_description='Long description',
-    keywords='some, keywords',
-    author='yourname',
-    author_email='yourmail',
-    license='GPLv2',
+    description='Dap archives loader and checker',
+    long_description='Module that loads a dap file, check it for '
+                     'sanity/validity and provide access for metadata via'
+                     'a class.',
+    keywords='devassiatnt, lint, dap',
+    author='Miro Hrončok',
+    author_email='miro@hroncok.cz',
+    license='GPLv2+',
     packages=find_packages(),
     classifiers=['Development Status :: 3 - Alpha',
                  'Intended Audience :: Developers',
                  'License :: OSI Approved :: '
-                 'GNU General Public License v2 (GPLv2)',
+                 'GNU General Public License v2 or later (GPLv2+)',
                  'Operating System :: POSIX :: Linux',
                  'Programming Language :: Python',
+                 'Programming Language :: Python :: 2',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.3',
                  ]
 )
