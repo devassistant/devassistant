c16bb89074bbddd2b3af73ee8ad900b122a05c99
Miro Hrončok <miro@hroncok.cz>
2014-01-06 18:45:41 +0100

    daploader: Initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup, find_packages
+except:
+    from distutils.core import setup, find_packages
+
+setup(
+    name = 'python-daploader',
+    version = '0.0.1',
+    description = 'Short description',
+    long_description = 'Long description',
+    keywords = 'some, keywords',
+    author = 'yourname',
+    author_email = 'yourmail',
+    license = 'GPLv2',
+    packages = find_packages(),
+    classifiers = ['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python',
+                  ]
+)
