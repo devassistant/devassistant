1e30ffc238ee35040c35d5a6b7fdff8874380989
Miro Hrončok <miro@hroncok.cz>
2014-01-07 21:39:40 +0100

    daploader: Test more things with meta_only.dap

--- test/test_dap.py
+++ test/test_dap.py
@@ -23,9 +23,13 @@ class TestDap(object):
         with pytest.raises(DapMetaError):
             Dap('test/no_meta.dap')
 
-    def test_meta_contents(self):
-        '''Data from meta.yaml should be in _meta dict'''
-        dap = Dap('test/meta_only.dap')
+    def test_dap_data(self):
+        '''Dap should have correct content in meta, basename and files'''
+        name = 'meta_only'
+        basename = name + '.dap'
+        dap = Dap('test/'+basename)
         assert dap.meta['package_name'] == 'foo'
         assert dap.meta['version'] == '1.0.0'
         assert u'Hrončok' in dap.meta['authors'][0]
+        assert dap.basename == basename
+        assert dap.files == [name, name + '/meta.yaml']
