61d9a64d76e2b8b2eda4e641708c0e84f3920ca0
Miro Hrončok <miro@hroncok.cz>
2014-01-08 17:41:53 +0100

    daploader: Typo

--- daploader/daplint.py
+++ daploader/daplint.py
@@ -7,7 +7,7 @@ def daplint():
     '''This parses command line arguments and let the user run daplint'''
     parser = OptionParser(usage='usage: %prog [options] dap_file [dap_file2...]', version='%prog 0.0.1')
     parser.add_option('-n', '--no-network', action='store_false', dest='network',
-                      default=True, help='don\'t perform checks that require internet connection')
+                      default=True, help='don\'t perform checks that require Internet connection')
     options, args = parser.parse_args()
     exitcode = 0
     if not args:
