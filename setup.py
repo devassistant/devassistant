7b4669add957d07eeecff922360828c2a79bae75
Miro Hrončok <miro@hroncok.cz>
2014-01-21 15:02:54 +0100

    daploader: setup.py test now runs py.test

--- setup.py
+++ setup.py
@@ -2,11 +2,71 @@
 # -*- coding: utf-8 -*-
 
 try:
-    from setuptools import setup, find_packages
+    from setuptools import setup, find_packages, Command
 except:
-    from distutils.core import setup, find_packages
+    from distutils.core import setup, find_packages, Command
+
+import subprocess
+import os
 
 from daploader import __version__
+
+
+class PyTest(Command):
+    user_options = [('test-runner=',
+                     't',
+                     'test runner to use; by default, multiple py.test runners are tried')]
+    command_consumes_arguments = True
+
+    def initialize_options(self):
+        self.test_runner = None
+        self.args = []
+
+    def finalize_options(self):
+        pass
+
+    def runner_exists(self, runner):
+        syspaths = os.getenv('PATH').split(os.pathsep)
+        for p in syspaths:
+            if os.path.exists(os.path.join(p, runner)):
+                return True
+
+        return False
+
+    def run(self):
+        # only one test runner => just run the tests
+        runners = ['py.test-2.7', 'py.test-3.3']
+        if self.test_runner:
+            runners = [self.test_runner]
+
+        have_runner = False
+        for runner in runners:
+            if self.runner_exists(runner):
+                have_runner = True
+
+        if not have_runner:
+            if self.runner_exists('py.test'):
+                runners = ['py.test']
+            else:
+                print('No py.test runners found, can\'t run tests.')
+                print('Tried: {0}.'.format(', '.join(runners + ['py.test'])))
+                raise SystemExit(100)
+
+        for runner in runners:
+            if len(runners) > 1:
+                print('\n' * 2)
+            print('Running tests using "{0}":'.format(runner))
+
+            retcode = 0
+            cmd = [runner]
+            for a in self.args:
+                cmd.append(a)
+            cmd.append('test')
+            t = subprocess.Popen(cmd)
+            rc = t.wait()
+            retcode = t.returncode or retcode
+
+        raise SystemExit(retcode)
 
 setup(
     name='daploader',
@@ -30,5 +90,6 @@ setup(
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3',
                  'Programming Language :: Python :: 3.3',
-                 ]
+                 ],
+    cmdclass={'test': PyTest}
 )
