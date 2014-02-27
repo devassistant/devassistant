68ee5576c2515c43159fa5c394f74704e803f7d8
Slavek Kabrda <bkabrda@redhat.com>
2014-02-27 10:14:49 +0100

    Fix excepthook after introducing eval_{exec,literal}_section functions

--- devassistant/excepthook.py
+++ devassistant/excepthook.py
@@ -23,9 +23,9 @@ class DAPrettyPrinter(pprint.PrettyPrinter):
 
 def is_local_subsection(command_dict):
     """Returns True if command dict is "local subsection", meaning
-    that it is "if", "for" or "scl" (not a real call, but calls
+    that it is "if", "else" or "for" (not a real call, but calls
     run_section recursively."""
-    for local_com in ['if ', 'for ', 'scl ']:
+    for local_com in ['if ', 'for ', 'else ']:
         if list(command_dict.keys())[0].startswith(local_com):
             return True
     return False
@@ -35,9 +35,10 @@ def excepthook(type, value, traceback):
     print('DevAssistant traceback (most recent call last):')
     curr_tb = traceback
     run_section_frames = []
+    record_frames_in = ['eval_exec_section', 'eval_literal_section']
     while curr_tb:
         if 'lang.py' in curr_tb.tb_frame.f_code.co_filename and \
-            curr_tb.tb_frame.f_code.co_name == 'run_section':
+            curr_tb.tb_frame.f_code.co_name in record_frames_in:
             run_section_frames.append(curr_tb.tb_frame)
 
         curr_tb = curr_tb.tb_next
@@ -46,7 +47,7 @@ def excepthook(type, value, traceback):
         pp = DAPrettyPrinter()
         for frame in run_section_frames:
             current_command_dict = frame.f_locals['command_dict']
-            # skip 'if', 'for' and 'scl' commands
+            # skip 'if', 'else' and 'for' commands
             # they call run_section recursively, but are still in the same 'run*' section
             if not is_local_subsection(current_command_dict):
                 print('File {0}:'.format(frame.f_locals['kwargs']['__sourcefiles__'][-1]))
