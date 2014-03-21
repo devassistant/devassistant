eff929aedf0346f14eabffb7e300adab79eda55d
Slavek Kabrda <bkabrda@redhat.com>
2014-03-21 15:02:14 +0100

    Revert "Add argparse argument that allows parsing remainder of CL invocation as arguments for appendable snippets"
    This reverts commit aeca969fbbf2375de8fca900fe39edea15b30787.

--- devassistant/cli/argparse_generator.py
+++ devassistant/cli/argparse_generator.py
@@ -93,9 +93,6 @@ class ArgparseGenerator(object):
             subparsers = p.add_subparsers(dest=settings.SUBASSISTANT_N_STRING.format(level),
                                           title=cls.subparsers_str,
                                           description=cls.subparsers_no_avail)
-        else:
-            # add argparse.REMAINDER to parse rest of the args for appendable snippets
-            p.add_argument('appendables_args', nargs='*')
 
     @classmethod
     def add_action_to(cls, parser, action, subactions, level):
