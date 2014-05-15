98b7b7addaae31137b4039172bbead68e0f34977
Tomas Radej <tradej@redhat.com>
2014-05-15 16:28:37 +0200

    More test for YUM pm

--- test/test_package_managers.py
+++ test/test_package_managers.py
@@ -1,14 +1,23 @@
 import pytest
+import six
 
 from flexmock import flexmock
 
+from devassistant.exceptions import ClException, DependencyException
 from devassistant.command_helpers import ClHelper
 from devassistant.package_managers import YUMPackageManager
+
+def yum_not_available():
+    try:
+        import yum
+        return False
+    except ImportError:
+        return True
 
 class TestYUMPackageManager(object):
 
     def setup_method(self, method):
-        self.ypm = YUMPackageManager()
+        self.ypm = YUMPackageManager
 
     @pytest.mark.parametrize('result', [True, False])
     def test_is_rpm_installed(self, result):
@@ -23,5 +32,53 @@ class TestYUMPackageManager(object):
     def test_is_group_installed(self, group, output, result):
         flexmock(ClHelper).should_receive('run_command')\
                 .with_args('yum group list "{grp}"'.format(grp=group)).and_return(output)
-        assert self.ypm.is_group_installed(group) is result
+        assert self.ypm.is_group_installed(group) == result
+
+    def test_was_rpm_installed(self):
+        pass
+
+    def test_install(self):
+        pkgs = ('foo', 'bar', 'baz')
+
+        flexmock(ClHelper).should_receive('run_command')
+        assert self.ypm.install(*pkgs) == pkgs
+
+        flexmock(ClHelper).should_receive('run_command').and_raise(ClException(None, None, None))
+        assert self.ypm.install(*pkgs) is False
+
+    def test_works(self):
+        mock = flexmock(six.moves.builtins)
+        mock.should_call('__import__')
+
+        mock.should_receive('__import__')
+        assert self.ypm.works()
+        mock.should_receive('__import__').and_raise(ImportError)
+        assert not self.ypm.works()
+
+    @pytest.mark.parametrize(('method', 'string', 'expected'), [
+        ('is_group_installed', '@foo', True),
+        ('is_rpm_installed', 'foo', True),
+        ('is_group_installed', 'foo', False),
+        ('is_rpm_installed', '@foo', False),
+    ])
+    def test_is_pkg_installed(self, method, string, expected):
+        flexmock(YUMPackageManager).should_receive(method).and_return(expected)
+        assert YUMPackageManager.is_pkg_installed(string) is expected
+
+    @pytest.mark.skipif(yum_not_available(), reason='Requires yum module')
+    def test_resolve(self):
+        import yum
+        expected = ['bar', 'baz']
+        fake_pkgs = [flexmock(po=flexmock(ui_envra=name)) for name in expected]
+        fake_yumbase = flexmock(setCacheDir=lambda x: True,
+                                selectGroup=lambda x: True,
+                                install=lambda x: True,
+                                returnPackageByDep=lambda x: True,
+                                resolveDeps=lambda: True,
+                                tsInfo=flexmock(getMembers=lambda: fake_pkgs))
+        flexmock(yum.YumBase).new_instances(fake_yumbase)
+        assert self.ypm.resolve('foo') == expected
+        fake_yumbase.should_receive('resolveDeps').and_raise(yum.Errors.PackageSackError)
+        with pytest.raises(DependencyException):
+            self.ypm.resolve('foo')
 
