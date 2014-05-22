0c18edd33d60e1c17d23a519a95ecbe72ac98f60
Tomas Radej <tradej@redhat.com>
2014-05-22 10:26:01 +0200

    Reworked generating manuals for GUI too

--- manpages/generate_man.py
+++ manpages/generate_man.py
@@ -1,18 +1,25 @@
 #!/usr/bin/python
 
-TEMPLATE = 'template.txt'
-
-def generate_man(template, binary, args):
+def generate_man(template, args):
+    if 'binary' not in args:
+        raise Exception('Binary name must be specified, it is used as file name.')
     with open(template, 'r') as temp:
         text = temp.read()
-    with open('{binary}.1'.format(binary=binary), 'w') as output:
-        output.write(text.format(binary=binary, **args))
+    with open('{binary}.1'.format(binary=args['binary']), 'w') as output:
+        output.write(text.format(**args))
 
 if __name__ == '__main__':
 
-    values = [('da',           {'desc':    '',
+    values = [('da.txt',       {'binary':  'da',
+                                'desc':    '',
                                 'seealso': ''}),
-              ('devassistant', {'desc':    'Please note that the \\fBda\\fP command is the recommended way of running DevAssistant (even though it works exactly the same).\n\n',
-                                'seealso': '.BR da (1)\n'})]
-    for (binary, args) in values:
-        generate_man(TEMPLATE, binary, args)
+              ('da.txt',       {'binary':  'devassistant',
+                                'desc':    'Please note that the \\fBda\\fP command is the recommended way of running DevAssistant (even though it works exactly the same).\n\n',
+                                'seealso': '.BR da (1)\n'}),
+              ('da-gui.txt',   {'binary':  'da-gui',
+                                'cli-bin': 'da'}),
+              ('da-gui.txt',   {'binary':  'devassistant-gui',
+                                'cli-bin': 'devassistant'})]
+
+    for (template, args) in values:
+        generate_man(template, args)
