c588c2da84ec0c1949d8129e7bcc4aa01936e13e
Slavek Kabrda <bkabrda@redhat.com>
2014-06-17 17:48:49 +0200

    Make features more story-like, add more testing assistants

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -12,12 +12,16 @@ def startgui(context):
 def just_start_it(context):
     startgui(context)
 
+@when('user clicks "{tab}" tab')
+def click_tab(context, tab):
+    context.dagui_root.child(tab).click()
+
 @then('"{tab}" is the current tab')
 @given('"{tab}" is the current tab')
 def tab_is_current(context, tab):
     assert context.dagui_root.child(tab).isSelected
 
-@then('"{fullname}" assistant should be displayed')
+@then('"{fullname}" assistant should be listed')
 def assistant_is_displayed(context, fullname):
-    assert context.dagui_root.child('Foo')
+    assert context.dagui_root.child(fullname)
     # TODO: how to text that the icon is there?
--- features/tabs.feature
+++ features/tabs.feature
@@ -2,4 +2,13 @@ Feature: Tabs switching
 
   Scenario: "Create Project" tab is default
     Given da-gui has just started
+    # TODO: is "is the current tab" vs. "assistant should" the right way to say this in BDD?
      Then "Create Project" is the current tab
+      and "Creator 1" assistant should be listed
+
+
+  Scenario: Tab switching works
+    Given da-gui is running
+     When user clicks "Modify Project" tab
+     Then "Modify Project" is the current tab
+      and "Modifier 1" assistant should be listed
