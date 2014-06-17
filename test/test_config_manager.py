c2eb6198508d20ded6c65ffa7ab1c639aec9af5c
Jaromír Končický <jkoncick@redhat.com>
2014-06-17 10:31:55 +0200

    Remade test_config_manager again

--- test/test_config_manager.py
+++ test/test_config_manager.py
@@ -1,60 +1,31 @@
 import os
 import sys
 import six
+import re
 import pytest
+import tempfile
 from flexmock import flexmock
-from devassistant import settings
-
-settings.CONFIG_FILE = '/dev/null'
 
 from devassistant.config_manager import ConfigManager
 
-test_lines = ["somekey=somevalue", "somebool=True", "bad\\=key=badvalue", "other\\\\bad\\=key=bad\\=val\\\\\\=ue"]
-test_filename = '/tmp/cmconfigfile'
-
-class FakeLogger(object):
-
-    def __init__(self):
-        self.contents = []
-
-    def warning(self, text):
-        self.contents.append(text)
-
-class FakeFileObject(object):
-
-    def __init__(self, contents):
-        self.contents = contents
-        self.i = 0;
-
-    def next(self):
-        if self.i < len(self.contents):
-            self.i = self.i + 1
-            return self.contents[self.i - 1]
-        raise StopIteration
-
-    def __next__(self):
-        return self.next()
-
-    def __iter__(self):
-        return self
-
-    def write(self, line):
-        self.contents.append(line.rstrip('\n'))
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, a, b, c):
-        pass
-
 class TestConfigManager(object):
 
-    def override_open(self, mode, contents):
-        fake_file_object = FakeFileObject(contents)
-        mock = flexmock(six.moves.builtins)
-        mock.should_call('open')
-        mock.should_receive('open').with_args('/dev/null', mode).and_return(fake_file_object)
-        return fake_file_object
+    def create_tempfile(self, contents):
+        tmp = tempfile.NamedTemporaryFile(mode='w', delete=False)
+        for line in contents:
+            tmp.write(line + '\n')
+        self.tmpfile = tmp.name
+        return tmp.name
+
+    def setup_method(self, method):
+        self.cm = ConfigManager()
+        self.cm.config_file = '/foo/bar'
+        self.test_lines = ["somekey=somevalue", "somebool=True", "bad\\=key=badvalue", "other\\\\bad\\=key=bad\\=val\\\\\\=ue"]
+        self.tmpfile = None
+
+    def teardown_method(self, method):
+        if self.tmpfile and os.path.exists(self.tmpfile):
+            os.remove(self.tmpfile)
 
     @pytest.mark.parametrize(('key', 'setvalue', 'resetvalue', 'getvalue'), [
         ("somekey",  "somevalue",  None,  "somevalue"),
@@ -67,58 +38,61 @@ class TestConfigManager(object):
     ])
     def test_set_get_config_values(self, key, setvalue, resetvalue, getvalue):
         # Check if ConfigManager returns correct values
-        cm = ConfigManager()
-        cm.set_config_value(key, setvalue)
+        self.cm.set_config_value(key, setvalue)
         if resetvalue is not None:
-            cm.set_config_value(key, resetvalue)
-        assert cm.get_config_value(key) == getvalue
+            self.cm.set_config_value(key, resetvalue)
+        assert self.cm.get_config_value(key) == getvalue
 
     def test_save_correct_values(self):
-        cm = ConfigManager()
-        fake_write_object = self.override_open('w', [])
         # Set some normal config values
-        cm.set_config_value("somekey", "somevalue")
-        cm.set_config_value("somebool", True)
+        self.cm.set_config_value("somekey", "somevalue")
+        self.cm.set_config_value("somebool", True)
         # Set config values with '=' or '\' inside key or value
-        cm.set_config_value("bad=key", "badvalue")
-        cm.set_config_value("other\\bad=key", "bad=val\\=ue")
-        # Finally save configuration file. Check the saved config file.
-        cm.save_configuration_file()
-        lines = fake_write_object.contents
-        assert len(lines) == 4
-        assert "somekey=somevalue" in lines
-        assert "somebool=True" in lines
-        assert "bad\\=key=badvalue" in lines
-        assert "other\\\\bad\\=key=bad\\=val\\\\\\=ue" in lines
-
-    def test_save_file_exists(self):
-        # Check if ConfigManager creates physical file
-        cm = ConfigManager()
-        cm.config_file = test_filename
-        cm.set_config_value("somekey", "somevalue")
-        cm.set_config_value("somebool", True)
-        cm.save_configuration_file()
-        assert(os.path.exists(test_filename))
-        assert(os.path.getsize(test_filename)) == 32
+        self.cm.set_config_value("bad=key", "badvalue")
+        self.cm.set_config_value("other\\bad=key", "bad=val\\=ue")
+        # Create fake file object which should receive 'write's with given contents
+        wrt = flexmock(write=lambda l: True)
+        wrt.should_call('write').with_args("somekey=somevalue\n").once()
+        wrt.should_call('write').with_args("somebool=True\n").once()
+        wrt.should_call('write').with_args("bad\\=key=badvalue\n").once()
+        wrt.should_call('write').with_args("other\\\\bad\\=key=bad\\=val\\\\\\=ue\n").once()
+        # Override open() to return fake file object and os.path.exists() to return True
+        flexmock(six.moves.builtins).should_receive('open').with_args('/foo/bar', 'w').and_return(wrt).once()
+        flexmock(os.path).should_receive('exists').and_return(True)
+        # Finally save configuration file
+        self.cm.save_configuration_file()
 
     def test_load_nonexistent_file(self):
-        # Create new ConfigManager. It should have no config.
-        logger = FakeLogger()
-        cm = ConfigManager()
-        cm.logger = logger
-        cm.config_file = test_filename
-        assert not cm.config_dict
+        self.cm.logger = flexmock()
+        self.cm.logger.should_receive("warning").never()
+        flexmock(os.path).should_receive('exists').with_args('/foo/bar').and_return(False)
         # Attempt to load nonexistent config file. It should have no config and no error was logged.
-        cm.load_configuration_file()
-        assert not cm.config_dict
-        assert len(logger.contents) == 0
+        self.cm.load_configuration_file()
+        assert not self.cm.config_dict
+
+    def test_load_error(self):
+        self.cm.logger = flexmock()
+        self.cm.logger.should_receive("warning").with_args(re.compile('^Could not load configuration file')).once()
+        flexmock(six.moves.builtins).should_receive('open').with_args('/foo/bar', 'r').and_raise(IOError)
+        flexmock(os.path).should_receive('exists').with_args('/foo/bar').and_return(True)
+        # Attempt to load config file which cannot be read. It should have no config and error was logged.
+        self.cm.load_configuration_file()
+        assert not self.cm.config_dict
+
+    def test_save_error(self):
+        self.cm.logger = flexmock()
+        self.cm.logger.should_receive("warning").with_args(re.compile('^Could not save configuration file')).once()
+        flexmock(six.moves.builtins).should_receive('open').with_args('/foo/bar', 'w').and_raise(IOError)
+        flexmock(os.path).should_receive('exists').with_args('/foo/bar').and_return(False)
+        flexmock(os.path).should_receive('exists').with_args('/foo').and_return(True)
+        # Attempt to save config file which cannot be written. Error should be logged.
+        self.cm.save_configuration_file()
 
     def test_load_file(self):
         # Load config file. It should have all config.
-        cm = ConfigManager()
-        self.override_open('r', test_lines)
-        cm.load_configuration_file()
-        assert len(cm.config_dict) == 4
+        self.cm.config_file = self.create_tempfile(self.test_lines)
+        self.cm.load_configuration_file()
+        assert len(self.cm.config_dict) == 4
 
     @pytest.mark.parametrize(('key', 'getvalue'), [
         ("somekey",        "somevalue"),
@@ -129,10 +103,9 @@ class TestConfigManager(object):
     ])
     def test_loaded_values(self, key, getvalue):
         # Check if configuration loaded from file is correct
-        cm = ConfigManager()
-        self.override_open('r', test_lines)
-        cm.load_configuration_file()
-        assert cm.get_config_value(key) == getvalue
+        self.cm.config_file = self.create_tempfile(self.test_lines)
+        self.cm.load_configuration_file()
+        assert self.cm.get_config_value(key) == getvalue
 
     @pytest.mark.parametrize(('contents'), [
         (["somekey=somevalue", "malformedline", "next=line"]),
@@ -140,15 +113,12 @@ class TestConfigManager(object):
     ])
     def test_load_malformed_file(self, contents):
         # Try to load malformed file. It should fail.
-        logger = FakeLogger()
-        cm = ConfigManager()
-        cm.logger = logger
-        self.override_open('r', contents)
-        cm.load_configuration_file()
-        assert not cm.config_dict
-        assert len(logger.contents) == 1
-        assert "Malformed configuration file" in logger.contents[0]
-    
+        self.cm.config_file = self.create_tempfile(contents)
+        self.cm.logger = flexmock()
+        self.cm.logger.should_receive("warning").with_args(re.compile('^Malformed configuration file')).once()
+        self.cm.load_configuration_file()
+        assert not self.cm.config_dict
+
     @pytest.mark.parametrize(('key', 'setvalue', 'result'), [
         ( None,       None,        False),
         ("empty",     False,       False),
@@ -159,14 +129,9 @@ class TestConfigManager(object):
     ])
     def test_config_changed(self, key, setvalue, result):
         # Check if config_changed property behaves correctly
-        cm = ConfigManager()
-        self.override_open('r', test_lines)
-        cm.load_configuration_file()
+        self.cm.config_file = self.create_tempfile(self.test_lines)
+        self.cm.load_configuration_file()
         if key:
-            cm.set_config_value(key, setvalue)
-        assert cm.config_changed == result
-
-    def setup_method(self, method):
-        if os.path.exists(test_filename):
-            os.remove(test_filename)
+            self.cm.set_config_value(key, setvalue)
+        assert self.cm.config_changed == result
 
