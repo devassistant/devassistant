542f373f414eeececc07e8ca979b00ed69304f65
Miro Hrončok <miro@hroncok.cz>
2014-09-25 15:57:37 +0200

    Use cmp_to_key from functools if possible

--- test/dapi/test_dapver.py
+++ test/dapi/test_dapver.py
@@ -1,33 +1,34 @@
 import os
 import sys
 from devassistant.dapi import dapver
+try:
+    from functools import cmp_to_key
+except ImportError:
+    def cmp_to_key(mycmp):
+        '''Convert a cmp= function into a key= function
+        Taken from http://code.activestate.com/recipes/576653-convert-a-cmp-function-to-a-key-function/'''
+        class K(object):
+            def __init__(self, obj, *args):
+                self.obj = obj
 
+            def __lt__(self, other):
+                return mycmp(self.obj, other.obj) < 0
 
-def cmp_to_key(mycmp):
-    '''Convert a cmp= function into a key= function
-    Taken from http://code.activestate.com/recipes/576653-convert-a-cmp-function-to-a-key-function/'''
-    class K(object):
-        def __init__(self, obj, *args):
-            self.obj = obj
+            def __gt__(self, other):
+                return mycmp(self.obj, other.obj) > 0
 
-        def __lt__(self, other):
-            return mycmp(self.obj, other.obj) < 0
+            def __eq__(self, other):
+                return mycmp(self.obj, other.obj) == 0
 
-        def __gt__(self, other):
-            return mycmp(self.obj, other.obj) > 0
+            def __le__(self, other):
+                return mycmp(self.obj, other.obj) <= 0
 
-        def __eq__(self, other):
-            return mycmp(self.obj, other.obj) == 0
+            def __ge__(self, other):
+                return mycmp(self.obj, other.obj) >= 0
 
-        def __le__(self, other):
-            return mycmp(self.obj, other.obj) <= 0
-
-        def __ge__(self, other):
-            return mycmp(self.obj, other.obj) >= 0
-
-        def __ne__(self, other):
-            return mycmp(self.obj, other.obj) != 0
-    return K
+            def __ne__(self, other):
+                return mycmp(self.obj, other.obj) != 0
+        return K
 
 
 class TestDapver(object):
