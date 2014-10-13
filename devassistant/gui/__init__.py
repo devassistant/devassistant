fc4c4f7ba0186ce518bcb57fc353492b7d5c7ec5
Slavek Kabrda <bkabrda@redhat.com>
2014-10-13 14:35:45 +0200

    Remove da-yaml-gui, fixes #109

--- devassistant/gui/__init__.py
+++ devassistant/gui/__init__.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 
-GUI_MESSAGE = "Developer Assistant GUI requires a running X server."
+GUI_MESSAGE = "DevAssistant GUI requires a running X server."
 GUI_MESSAGE_DISPLAY = "Environment variable DISPLAY is not set."
 
 
@@ -26,19 +26,3 @@ def run_gui():
         sys.exit(1)
     from devassistant.gui import main_window
     main_window.MainWindow()
-
-
-def run_yaml_gui():
-    """
-    Function for running DevAssistant YAML Creator GUI
-    """
-    try:
-        from gi.repository import Gtk
-    except RuntimeError as e:
-        sys.stderr.write(GUI_MESSAGE)
-        sys.stderr.write("%s: %r" % (e.__class__.__name__, str(e)))
-        sys.stderr.flush()
-        sys.exit(1)
-
-    from devassistant.gui import creator_window
-    creator_window.CreatorWindow()
