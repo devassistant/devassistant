f905eff19d5552f11775f78183ab2b1e349a7808
Slavek Kabrda <bkabrda@redhat.com>
2014-10-17 16:40:24 +0200

    Make sure exit functions are run if DA is killed by SIGINT

--- devassistant/sigint_handler.py
+++ devassistant/sigint_handler.py
@@ -3,6 +3,7 @@ import sys
 
 from devassistant.logger import logger
 from devassistant import package_managers
+from devassistant import utils
 
 
 def override():
@@ -11,6 +12,7 @@ def override():
             logger.warning('Can\'t interrupt dependency installation!')
         else:
             logger.info('DevAssistant received SIGINT, exiting ...')
+            utils.run_exitfuncs()
             sys.exit(0)
 
     signal.signal(signal.SIGINT, signal_handler)
