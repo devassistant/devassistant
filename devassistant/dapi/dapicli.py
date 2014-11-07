5d2b289ac270b751ebb87f07eb6493f4fdb1bd01
Miro Hrončok <miro@hroncok.cz>
2014-11-07 12:01:35 +0100

    Remove _eshout, not using it anymore

--- devassistant/dapi/dapicli.py
+++ devassistant/dapi/dapicli.py
@@ -356,11 +356,6 @@ def install_dap(name, version='', update=False):
     except:
         pass
 
-def _eshout(e):
-    '''Prints the Exception's message to stderr'''
-    sys.stderr.write(str(e))
-    sys.stderr.write('\n')
-
 def get_dependency_metadata():
     '''Returns list of strings with dependency metadata from Dapi'''
     link = os.path.join(_api_url(),'meta.txt')
