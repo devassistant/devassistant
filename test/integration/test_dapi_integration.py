892fadba5473cf1cb2e15c792104903d446a831d
Miro Hrončok <miro@hroncok.cz>
2015-03-05 11:52:46 +0100

    Integration tests for installing daps

--- test/integration/test_dapi_integration.py
+++ test/integration/test_dapi_integration.py
@@ -1,11 +1,16 @@
 import pytest
+import os
 
+from test import fixtures_dir
 from test.integration.misc import run_da
 
+
+def dap_path(fixture):
+    '''Return appropriate dap path'''
+    return os.path.join(fixtures_dir, 'dapi', 'daps', fixture)
+
+
 class TestDAPIIntegration(object):
-    # most of the tests that make sense to write will only be sanely doable
-    #  after #316 is fixed, most importantly when dapicli respects DEVASSISTANT_HOME
-    #  and we can completely cut it from standard devassistant path
     @pytest.mark.webtest
     def test_installation_of_nonexistent_package(self):
         res = run_da('pkg install hope_there_is_never_dap_named_like_this', expect_error=True)
@@ -17,3 +22,12 @@ class TestDAPIIntegration(object):
     def test_info(self):
         # the actual output can change, so just test that this doesn't fail
         res = run_da('pkg info dap')
+
+    def test_install_local(self):
+        res = run_da('pkg install ' + dap_path('meta_only/foo-1.0.0.dap'))
+        assert 'INFO: Successfully installed DAPs foo' in res.stdout
+
+    @pytest.mark.webtest
+    def test_install_dapi(self):
+        res = run_da('pkg install common_args')
+        assert 'INFO: Successfully installed DAPs common_args' in res.stdout
